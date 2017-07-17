506a2b57e0818be9d0bb7651f971dcf70dd2145c
MCOfficer <mcofficer@gmx.de>
2017-07-17 21:47:21 +0200

    add greet on leave event

--- src/main/java/bot/MemberEventListener.java
+++ src/main/java/bot/MemberEventListener.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.Properties;
 
 import net.dv8tion.jda.core.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.core.events.guild.member.GuildMemberLeaveEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.entities.MessageChannel;
@@ -26,6 +27,16 @@ public class MemberEventListener extends ListenerAdapter
       MessageChannel channel = event.getGuild().getTextChannelsByName(MAIN_CHANNEL, true).get(0);
       TextChannel rules = event.getGuild().getTextChannelsByName("news", true).get(0);
       channel.sendMessageFormat("Hello %s! Welcome to the Endless Sky Community Discord. Make sure to read the %s and enjoy your stay! I'll be watching...", user.getAsMention(), rules).queue();
+    }
+  }
+
+  @Override
+  public void onGuildMemberLeave(GuildMemberLeaveEvent event)
+  {
+    User user = event.getMember().getUser();
+    if (!user.isBot()){
+      MessageChannel channel = event.getGuild().getTextChannelsByName(MAIN_CHANNEL, true).get(0);
+      channel.sendMessageFormat("%s has just left the server. Goodbye!", user).queue();
     }
   }
 
