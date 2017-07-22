1e0ea3cae56bbd7eaa5ed0a467a1de9a55741f06
MCOfficer <mcofficer@gmx.de>
2017-07-22 19:10:32 +0200

    added -apod command

--- src/main/java/bot/MiscCommands.java
+++ src/main/java/bot/MiscCommands.java
@@ -1,10 +1,24 @@
 package bot;
 
 import java.io.File;
+import java.io.FileReader;
+import java.lang.String;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import org.json.JSONObject;
+
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+import jdk.nashorn.api.scripting.ScriptObjectMirror;
+
+import javax.script.ScriptException;
+import java.io.IOException;
 
 import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
 import de.btobastian.sdcf4j.CommandHandler;
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.MessageBuilder;
@@ -38,10 +52,49 @@ implements CommandExecutor{
 					name = "shiptemplate.blend";
 				else
 					channel.sendMessage("Sorry, I only have templates for 'outfit', 'ship' and 'plugin'.").queue();
-				
+
 				if(name.length() > 0)
 					channel.sendFile(new File("data/templates/" + name), new MessageBuilder().append(" ").build()).queue();
 			}
 	}
+
+	@Command(aliases = {"-apod"}, description = "posts a random APOD(NASA's Astronomy Picture of the Day).", usage = "-apod")
+	public void onApodCommand(MessageChannel channel)
+	{
+		JSONObject json = null;
+		try{
+			javax.script.ScriptEngine engine = new ScriptEngineManager().getEngineByName("js");
+			ScriptObjectMirror result = (ScriptObjectMirror)engine.eval(new java.io.FileReader("scripts/random_apod.js"));
+			String sresult = result.values().toString();
+			sresult = sresult.substring(sresult.indexOf("[")+1, sresult.indexOf("]"));
+			URL url = new URL("https://api.nasa.gov/planetary/apod?api_key=Lk7O2c43wwYEUWZXHESdlARCbnSVs5gT0qgWGJKL&date=" + sresult);
+			json = getJson(url);
+		}
+		catch (ScriptException|IOException e){
+			e.printStackTrace(System.out);
+		}
+		EmbedBuilder eb = new EmbedBuilder();
+		if (json.getString("media_type").equals("image")){
+			eb.setImage(json.getString("hdurl"));
+			eb.setTitle(json.getString("title"));
+			eb.setDescription(json.getString("explanation"));
+			channel.sendMessage(eb.build()).queue();
+		}else if (json.getString("media_type").equals("video")){
+			channel.sendMessage(json.getString("url").replace("embed/", "watch?v=")).queue();
+		}else{
+			channel.sendMessage("Incompatible Media Type, please try again").queue();
+		}
+	}
+
+	private static JSONObject getJson(URL url) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+		StringBuilder sb = new StringBuilder();
+		int cp;
+		while ((cp = br.read()) != -1) {
+			sb.append((char) cp);
+	 }
+	 JSONObject json = new JSONObject(sb.toString());
+	 return json;
+ }
 
 }
