a53b1fdc8ccdefb310d69f339a5ed5c69c81de40
tehhowch <tehhowchmh@gmail.com>
2017-07-25 00:37:58 -0500

    Refactor "checkLookup" and fix newly-added bug

--- src/main/java/bot/LookupCommands.java
+++ src/main/java/bot/LookupCommands.java
@@ -109,15 +109,18 @@ implements CommandExecutor{
 			String path = "https://github.com/endless-sky/endless-sky/commit/" + request.toString().replace(" ","");
 			channel.sendMessage(path).queue();
 		}
-		else
+		else if(request.length() > 0){
 			channel.sendMessage("At least 7 characters are required.").queue();
+		}
+		else
+			channel.sendMessage("https://github.com/endless-sky/endless-sky/commit/").queue();
 	}
 
 
 
 	@Command(aliases = {"-lookup"}, description = "Shows the image and description of X.", usage = "-lookup X", privateMessages = true)
 	public void onLookupCommand(MessageChannel channel, String[] args){
-		if(args.length > 1){
+		if(args.length > 0){
 			String request = args[0];
 			for(int i = 1; i < args.length; ++i){
 				request += " " + args[i];
@@ -162,7 +165,7 @@ implements CommandExecutor{
 
 	@Command(aliases = {"-show"}, description = "Shows both image and all data associated with X.", usage = "-show X", privateMessages = true)
 	public void onShowCommand(MessageChannel channel, String[] args){
-		if(args.length > 1){
+		if(args.length > 0){
 			String request = args[0];
 			for(int i = 1; i < args.length; ++i){
 				request += " " + args[i];
@@ -197,7 +200,7 @@ implements CommandExecutor{
 
 	@Command(aliases = {"-showimage", "-showImage"}, description = "Shows image of X. Does not print data.", usage = "-showimage X", privateMessages = true)
 	public void onShowimageCommand(MessageChannel channel, String[] args){
-		if(args.length > 1){
+		if(args.length > 0){
 			String request = args[0];
 			for(int i = 1; i < args.length; ++i){
 				request += " " + args[i];
@@ -217,7 +220,7 @@ implements CommandExecutor{
 
 	@Command(aliases = {"-showdata", "-showData"}, description = "Shows data of X. Does not print images.", usage = "-showdata X", privateMessages = true)
 	public void onShowdataCommand(MessageChannel channel, String[] args){
-		if(args.length > 1){
+		if(args.length > 0){
 			String request = args[0];
 			for(int i = 1; i < args.length; ++i){
 				request += " " + args[i];
@@ -274,78 +277,34 @@ implements CommandExecutor{
 	// If helper is 'true', will try both as-passed 'lookup', and with
 	// enforced word capitalization.
 	private String checkLookup(String lookup, boolean helper){
-		if(data.contains("\nship \"" + lookup + "\"")){
-			return "\nship \"" + lookup + "\"";
+		final String[] dataTypes = {
+			"ship",
+			"outfit",
+			"mission",
+			"person",
+			"planet",
+			"system",
+			"effect",
+			"scene",
+			"fleet",
+			"event",
+			"government",
+			"phrase"
+		};
+		for(String str : dataTypes){
+			if(data.contains("\n" + str + " \"" + lookup + "\"")){
+				return "\n" + str + " \"" + lookup + "\"";
+			}
+			else if(data.contains("\n" + str + " " + lookup)){
+				return "\n" + str + " " + lookup;
+			}
 		}
-		else if(data.contains("\noutfit \"" + lookup + "\"")){
-			return "\noutfit \"" + lookup + "\"";
+		// The lookup may be exact:
+		if(data.contains("\n" + lookup)){
+			return "\n" + lookup;
 		}
-		else if(data.contains("\nmission \"" + lookup + "\"")){
-			return "\nmission \"" + lookup + "\"";
-		}
-		else if(data.contains("\nperson \"" + lookup + "\"")){
-			return "\nperson \"" + lookup + "\"";
-		}
-		else if(data.contains("\nplanet \"" + lookup + "\"")){
-			return "\nplanet \"" + lookup + "\"";
-		}
-		else if(data.contains("\nsystem \"" + lookup + "\"")){
-			return "\nsystem \"" + lookup + "\"";
-		}
-		else if(data.contains("\neffect \"" + lookup + "\"")){
-			return "\neffect \"" + lookup + "\"";
-		}
-		else if(data.contains("\tscene \"" + lookup + "\"")){
-			return "\tscene \"" + lookup + "\"";
-		}
-		else if(data.contains("\nfleet \"" + lookup + "\"")){
-			return "\nfleet \"" + lookup + "\"";
-		}
-		else if(data.contains("\nevent \"" + lookup + "\"")){
-			return "\nevent \"" + lookup + "\"";
-		}
-		else if(data.contains("\ngovernment \"" + lookup + "\"")){
-			return "\ngovernment \"" + lookup + "\"";
-		}
-		// The items may not be quoted in their definition.
-		else if(data.contains("\nship " + lookup)){
-			return"\nship " + lookup;
-		}
-		else if(data.contains("\noutfit " + lookup)){
-			return "\noutfit " + lookup;
-		}
-		else if(data.contains("\nmission " + lookup)){
-			return "\nmission " + lookup;
-		}
-		else if(data.contains("\nperson " + lookup)){
-			return "\nperson " + lookup;
-		}
-		else if(data.contains("\nplanet " + lookup + "\n")){
-			return "\nplanet " + lookup;
-		}
-		else if(data.contains("\nsystem " + lookup + "\n")){
-			return "\nsystem " + lookup;
-		}
-		else if(data.contains("\neffect " + lookup)){
-			return "\neffect " + lookup;
-		}
-		else if(data.contains("\tscene " + lookup)){
-			return "\tscene " + lookup;
-		}
-		else if(data.contains("\nfleet " + lookup)){
-			return "\nfleet " + lookup;
-		}
-		else if(data.contains("\nevent " + lookup)){
-			return "\nevent " + lookup;
-		}
-		else if(data.contains("\ngovernment " + lookup)){
-			return "\ngovernment " + lookup;
-		}
-		else if(data.contains("\n"+lookup)){
-			return "\n"+lookup;
-		}
+		// or maybe just not capitalized correctly.
 		else if(helper){
-			// Uppercase the first letter of words in the lookup string.
 			lookup = CapitalizeWords(lookup);
 			return checkLookup(lookup, false);
 		}
