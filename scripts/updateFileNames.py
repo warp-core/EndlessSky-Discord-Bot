985ad6ae37451df408b11d452fcf999758df6a06
M*C*O <22377202+MCOfficer@users.noreply.github.com>
2017-11-25 01:25:21 +0100

    windows compatibility for the python script

--- scripts/updateFileNames.py
+++ scripts/updateFileNames.py
@@ -24,6 +24,7 @@ backupPath = os.path.join(os.path.dirname(dataFilePath), 'dfn.old')
 authPath = os.path.abspath(os.path.join(scriptDir, *authRelPath.split('/')))
 
 if os.path.exists(dataFilePath):
+    os.remove(backupPath)
     os.rename(dataFilePath, backupPath)
 
 # Read OAuth token for GitHub, if possible.
